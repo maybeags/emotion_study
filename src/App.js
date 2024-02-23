c25112a3ba5ec0931dade8bb03dc6936789fcf67
maybeags <suren0709@gmail.com>
2024-02-23 17:28:14 +0900

    applying CSS jsx etc to make mypage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import { Reset } from 'styled-reset';
 import { Route, Routes } from 'react-router-dom';
 import SideBar from './components/SideBar/SideBar';
+import SideBarTop from './components/SideBarTop/SideBarTop';
+import RootLayout from './components/RootLayout/RootLayout';
+import Mypage from './pages/Mypage/Mypage';
 
 function App() {
   return (
     <>
       <Reset />
       <SideBar />
-      <Routes>
-
-        <Route path='/mypage' element={<>마이페이지1</>}/>
-        <Route path='/board' element={<>게시판</>}/>
-        <Route path='/notice' element={<>공지사항</>}/>
-
-      </Routes>
-
+      <SideBarTop />
+      <RootLayout>
+        <Routes>
+          <Route path='/mypage' element={<Mypage />}/>
+          <Route path='/board' element={<>게시판</>}/>
+          <Route path='/notice' element={<>공지사항</>}/>
+        </Routes>
+      </ RootLayout>
     </>
   );
 }
--- src/components/SideBarTop/style.js
+++ src/components/SideBarTop/style.js
@@ -3,32 +3,32 @@ import { css } from "@emotion/react";
 export const layout = (isShow) => css`
     box-sizing: border-box;
     position: fixed;
-    top: 0;
-    left: ${isShow ? "0px" : "-300px"};
+    top: ${isShow ? "-82px" : "0px"};
+    right: 0px;
     z-index: 99;
     border-right: 1px solid #dbdbdb;
-    width: 300px;
-    height: 100%;
-    transition: left 0.5s ease-in-out;
+    width: 50%;
+    height: 80px;
+    transition: top 0.5s ease-in-out;
     background-color: white;
-    box-shadow: 1px 0px 3px #00000022;
+    box-shadow: 0px 0px 3px 1px #000022;
 `;
 
 export const toggleButton = css`
     box-sizing: border-box;
     position: absolute;
-    transform: translateY(-50%);
-    top: 50%;
-    right: -15px;
+    transform: translateX(-100%);
+    top: 80px;
+    right: -5.2%;
     display: flex;
     justify-content: center;
     align-items: center;
     padding: 0;
     border: 1px solid #dbdbdb;
-    border-top-right-radius: 8px;
+    border-bottom-left-radius: 8px;
     border-bottom-right-radius: 8px;
-    width: 15px;
-    height: 50px;
+    width: 50px;
+    height: 20px;
     background-color: white;
     cursor: pointer;
     &:hover {
@@ -41,9 +41,10 @@ export const toggleButton = css`
 
 export const menuList = css`
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    justify-content: center;
     align-items: center;
-    padding: 20px 0px;
+    height: 100%;
 `;
 
 export const menuItem = css`
@@ -51,17 +52,16 @@ export const menuItem = css`
     display: flex;
     justify-content: center;
     align-items: center;
+    margin: 0px 10px;
 
-    border-bottom: 1px solid #dbdbdb;
-    width: 100%;
+    border: 1px solid #dbdbdb;
+    border-radius: 5px;
+    width: 200px;
     height: 50px;
     color: black;
     font-weight: 600;
     text-decoration: none;
     cursor: pointer;
-    &:nth-of-type(1) {
-        border-top: 1px solid #dbdbdb;
-    }
     &:hover {
         background-color: #eee;
     }
